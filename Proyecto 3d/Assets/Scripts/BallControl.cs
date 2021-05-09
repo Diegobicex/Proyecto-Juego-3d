1796b3c8e17dc67befb11f8f8d8ba8d2923e7f9b
juan Diego <diego.jdcc@gmail.com>
2021-05-09 13:09:38 +0200

    Ver 0.2
    Añadidos control de la bola y cámara modificada

---
+++ Proyecto 3d/Assets/Scripts/BallControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    //variables públicas    
+    public float ballForce = 1.0f;
+
+    //variables privadas
+    private Vector3 horizontalInput, verticalinput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vamos a empujar la bola en la dirección que pulse el usuario
+
+        //primero calculamos el input horizontal y vertical
+
+        horizontalInput = transform.right * Input.GetAxis("Horizontal");
+        verticalinput = transform.forward * Input.GetAxis("Vertical");
+
+        //empujamos la bola con la fuerza ballforce
+        GetComponent<Rigidbody>().AddForce( (horizontalInput+verticalinput) * ballForce );
+    }
+}
